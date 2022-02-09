6f56fa77737a34b0c61c287249a75ca798365104
Lucas lira Ferreira <72422365+lucas81303518@users.noreply.github.com>
2022-02-09 02:07:13 -0300

    Alterações triggers tabela Pedido

--- Cad_Cliente/Comandos Sql/Criar triggers.sql
+++ Cad_Cliente/Comandos Sql/Criar triggers.sql
@@ -1,97 +1,21 @@
--- Criando trigger para cada vez que adicione um pedido ele atualize os valores da tabela finanças
-delimiter $$
-create trigger Financas_TR_Insert after insert
-on Pedido
-for each row
-begin
-update Financas 
-set Vendas_Total = (select sum(p.Valor_Total) from Pedido p);
-
-update Financas 
-set Custo_Total = (select sum(p.Custo) from Pedido p);
-
-update Financas 
-set Lucro_Total = (select (sum(p.Valor_Total)-sum(p.Custo)) from Pedido p);
-
-update Financas 
-set Maluart_50 = (select ((sum(p.Valor_Total)-sum(p.Custo))*0.5) from Pedido p);
-
-update Financas 
-set Salario_30 = (select ((sum(p.Valor_Total)-sum(p.Custo))*0.3) from Pedido p);
-
-update Financas 
-set Investimento_20 = (select ((sum(p.Valor_Total)-sum(p.Custo))*0.2) from Pedido p);
-end$$
-delimiter ;
-
--- Criando trigger para cada vez que Edite um pedido ele atualize os valores da tabela finanças
-delimiter $$
-create trigger Financas_TR_Update after update
-on Pedido
-for each row
-begin
-update Financas 
-set Vendas_Total = (select sum(p.Valor_Total) from Pedido p);
-
-update Financas 
-set Custo_Total = (select sum(p.Custo) from Pedido p);
-
-update Financas 
-set Lucro_Total = (select (sum(p.Valor_Total)-sum(p.Custo)) from Pedido p);
-
-update Financas 
-set Maluart_50 = (select ((sum(p.Valor_Total)-sum(p.Custo))*0.5) from Pedido p);
-
-update Financas 
-set Salario_30 = (select ((sum(p.Valor_Total)-sum(p.Custo))*0.3) from Pedido p);
-
-update Financas 
-set Investimento_20 = (select ((sum(p.Valor_Total)-sum(p.Custo))*0.2) from Pedido p);
-end$$
-delimiter ;
-
--- Criando trigger para cada vez que delete um pedido ele atualize os valores da tabela finanças
-delimiter $$
-create trigger Financas_TR_Delete after delete
-on Pedido
-for each row
-begin
-update Financas 
-set Vendas_Total = (select sum(p.Valor_Total) from Pedido p);
-
-update Financas 
-set Custo_Total = (select sum(p.Custo) from Pedido p);
-
-update Financas 
-set Lucro_Total = (select (sum(p.Valor_Total)-sum(p.Custo)) from Pedido p);
-
-update Financas 
-set Maluart_50 = (select ((sum(p.Valor_Total)-sum(p.Custo))*0.5) from Pedido p);
-
-update Financas 
-set Salario_30 = (select ((sum(p.Valor_Total)-sum(p.Custo))*0.3) from Pedido p);
-
-update Financas 
-set Investimento_20 = (select ((sum(p.Valor_Total)-sum(p.Custo))*0.2) from Pedido p);
-end$$
-delimiter ;
-
--- Criando Trigger para cada vez que inserir um pedido calcule o valor restante do Pedido
+-- Criando Trigger para cada vez que inserir um pedido calcule o valor do lucro e restante do Pedido
 delimiter $$
 create trigger Pedido_TR_Insert before insert
 on Pedido
 for each row
 begin
 set new.Restante = (new.Valor_Total - new.Valor_Pago);
+set new.Lucro = (new.Valor_Total - new.Custo);
 end$$
 delimiter ;
 
--- Criando Trigger para cada vez que Editar um pedido atualize o valor restante do pedido
+-- Criando Trigger para cada vez que Editar um pedido atualize o valor do lucro e restante do pedido
 delimiter $$
 create trigger Pedido_TR_Update before update
 on Pedido
 for each row
 begin
 set new.Restante = (new.Valor_Total - new.Valor_Pago);
+set new.Lucro = (new.Valor_Total - new.Custo);
 end$$
 delimiter ;
